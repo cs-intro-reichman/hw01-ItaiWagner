20374618eec241943eae449f2464407363be1002
Itai Wagner <itai.wagner@post.runi.ac.il>
2023-12-20 13:54:28 +0200

    HW1 final

--- GenThree.java
+++ GenThree.java
@@ -8,12 +8,16 @@ public class GenThree {
 		int a = Integer.parseInt(args[0]);
 		int b = Integer.parseInt(args[1]);
 		
+		// assigining the a and b to min/max vars
+		int minVar = Math.min(a, b);
+		int maxVar = Math.max(a, b);
+		
 		// Calculating 3 numbers using the random function by multiplying it by the (b - a) and prints them
-		int randomNumber1 = (int) ((b - a)*(Math.random())) + a;
+		int randomNumber1 = (int) ((maxVar - minVar)*(Math.random())) + minVar;
 		System.out.println(randomNumber1);
-		int randomNumber2 = (int) ((b - a)*(Math.random())) + a;
+		int randomNumber2 = (int) ((maxVar - minVar)*(Math.random())) + minVar;
 		System.out.println(randomNumber2);
-		int randomNumber3 = (int) ((b - a)*(Math.random())) + a;
+		int randomNumber3 = (int) ((maxVar - minVar)*(Math.random())) + minVar;
 		System.out.println(randomNumber3);
 		
 		// Calculate who is the minimal number
